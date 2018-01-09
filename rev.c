96ee7de115d989782a657742a5e7e7e65d64e76f
DHARSHINIMURUGESAN <31503136+DHARSHINIMURUGESAN@users.noreply.github.com>
2018-01-09 22:05:33 +0530

    Rename aa.c to rev.c

---
+++ rev.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int n,num,rev=0;
+printf("enter the number");
+scanf("%d",&n);
+num=n;
+while(n! = 0)
+{
+rev = (rev * 10) + (n % 10);
+n = n/10;
+}
+if(rev == num)
+{
+printf("%d is palindrome.",num);
+}
+else
+{
+printf("%d is not palindrome.",num);
+}
+return 0;
+}
