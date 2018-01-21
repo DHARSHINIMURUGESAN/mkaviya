e6b361f6fbf86930b50863b44c37b33255083ce6
DHARSHINIMURUGESAN <31503136+DHARSHINIMURUGESAN@users.noreply.github.com>
2018-01-21 10:33:27 +0530

    Create arr.c

---
+++ arr.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n,k,a[1],a[2];
+printf("enter the numbers");
+scanf("%d",&n);
+for(i=1;i<=n;++i)
+{
+k=a[1]+a[2];
+}
+printf("the sum of the first k integer%d",k);
+return 0;
+}
