a4889f1ae350773cea3c6e14c2bf04879d2cc50f
DHARSHINIMURUGESAN <31503136+DHARSHINIMURUGESAN@users.noreply.github.com>
2018-02-01 08:52:02 +0000

    Create among.cpp

---
+++ among.cpp
@@ -0,0 +1,21 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int a[10],t,j,i,n;
+cout<<"enter 10 num";
+for(i=0;i<10;i++)
+{
+for(j=i;j<10;j++)
+{
+if(a[i]<a[j])
+{
+t=a[i];
+a[i]=a[j];
+a[j]=t;}
+}
+}
+}
+cout<<"\n max num among 10"<<a[0];
+return 0;
+}
