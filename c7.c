60dc9b9edc2bd2de882ebc2b166caabfd21c08ee
DHARSHINIMURUGESAN <31503136+DHARSHINIMURUGESAN@users.noreply.github.com>
2018-02-01 08:39:01 +0000

    Create c7.c

---
+++ c7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+  char a[50];
+  int i,n;
+  printf("enter the character");
+  scanf("%s",&a);
+  for(i=0;i<=a[50];i++)
+  {
+      if(i!=0)
+      {
+          n=n+1;
+          printf("print the special character")
+      }
+  }
+
+    return 0;
+}
