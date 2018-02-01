d793c5c1f4e495582414e3409d866f5bc0b6bbc0
DHARSHINIMURUGESAN <31503136+DHARSHINIMURUGESAN@users.noreply.github.com>
2018-02-01 08:58:11 +0000

    Create fib7.c

---
+++ fib7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n1,t1,t2,i,n;
+printf("enter the number");
+scanf("%d",&n);
+for(i=1;i<n;i++)
+{
+printf("enter t1);
+n1=t1+t2;
+t1=t2;
+t2=n1;
+}
+return 0;
+}
